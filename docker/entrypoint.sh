1c9d0ee065996ef6ff0bb718d08539181b31cb84
snowstorm-alfredosalata <as@salata.ovh>
2024-03-19 11:42:09 +0100

    feat: Added basic logging and Gunicorn config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-gunicorn -b "0.0.0.0:8000" -w ${GUNICORN_WORKERS} "filegateway.app:setup_app()"
+gunicorn -b "0.0.0.0:8000" -w ${GUNICORN_WORKERS} "filegateway.app:setup_app()" \
+    --access-logfile ${GUNICORN_ACCESS_LOG_FILE} \
+    --log-file ${GUNICORN_LOG_FILE} \
+    --log-level ${GUNICORN_LOG_LEVEL} 
